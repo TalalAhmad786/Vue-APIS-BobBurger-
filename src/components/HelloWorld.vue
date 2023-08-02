d42108f62f754bc13d20dabe1865e9dc0a61d7ef
TalalAhmad786 <99980289+TalalAhmad786@users.noreply.github.com>
2023-08-03 02:03:04 +0500

    Final Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,48 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{msg}}</h1> 
+
+    <v-container>   
+      <v-row>
+        <v-col cols="6" md="4" v-for="udata in user" :key="udata.id">
+    <v-card class="ma-4 pa-4"  elevation="8" style="width: 300px; height: 400px;">
+            <img class="w-50" :src="udata.image">
+            <v-card-title class="text-left text-wrap">{{`Name : ${ udata.name}, (${udata.id})`}}</v-card-title>
+            <p class="text-left text-wrap">{{ udata.gender }}</p>
+            <p class="text-left text-wrap">{{ udata.occupation }}</p>
+
+                
+    </v-card>
+  </v-col>
+  </v-row>
+  </v-container>
+
+    
+    
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed:{
+    ...mapGetters({user: 'getUsers'})
+  },
+  methods: {
+    ...mapActions({createUser: 'registerUser'})
+  },
+  created() {
+    this.createUser();
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    user: {},
+    users: []
   },
   mutations: {
+    SET_USER (state, payload) {
+      state.user = payload
+    },
+    SET_USERS (state, payload) {
+      state.users = payload
+    }
   },
   actions: {
+    async registerUser ({ commit }) {
+      const response = await axios.get(
+        'https://bobsburgers-api.herokuapp.com/v2/characters'
+      )
+      console.log(response)
+      commit('SET_USER', response.data)
+      }
   },
-  modules: {
+  getters: {
+    getUsers (state) {
+      return state.user
+    }
   }
 })
